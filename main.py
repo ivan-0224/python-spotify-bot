d2a58409fe36466c45d02b265e6c4218e33a1e47
ivan-0224 <ivanmiluticev@gmail.com>
2025-05-05 22:46:57 +0300

    update:main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import telebot
 from telegram import BOT_TOKEN, CLIENT, song_callback_query,\
     album_callback_query, new_message, artist_callback_query,\
     playlist_callback_query
 
 if __name__ == '__main__':
-    print('[BOT] Starting...')
-    CLIENT.start(bot_token=BOT_TOKEN)
-    CLIENT.run_until_disconnected()
+    try:
+        print('[BOT] Starting...')
+        CLIENT.start(bot_token=BOT_TOKEN)
+        CLIENT.run_until_disconnected()
+    except Exception as e:
+        print(f'[BOT] Error: {e}')
