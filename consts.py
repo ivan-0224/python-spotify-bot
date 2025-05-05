c9e37daab9b98cf821726dd62a5bbc33a5138707
ivan-0224 <ivanmiluticev@gmail.com>
2025-05-05 22:37:11 +0300

    update: const.py

--- consts.py
+++ consts.py
@@ -1,35 +1,37 @@
-WELCOME_MESSAGE = '''Hi,
-A Simple Telegram Bot That Lets You Download Tracks From Spotify And YouTube!
-Supports Downloading Tracks, Albums, Playlists, Artists! 🎧📱💿'''
+# Welcome message
+WELCOME_MESSAGE = (
+    "Hi,\n"
+    "A Simple Telegram Bot That Lets You Download Tracks From Spotify And YouTube!\n"
+    "Supports Downloading Tracks, Albums, Playlists, Artists! 🎧📱💿"
+)
 
-ARTISTS_MESSAGE = '''Please send the name of the artist like this: Artist Name'''
+# Messages for user input
+ARTISTS_MESSAGE = "Please send the name of the artist like this: Artist Name"
+SINGLE_MESSAGE = (
+    "Please send the name of the song like this:\n"
+    "Song Name\n"
+    "or for better search, use this format:\n"
+    "Song Name - Artist Name"
+)
+ALBUM_MESSAGE = (
+    "Please send the name of the album like this:\n"
+    "Album Name\n"
+    "or for better search, use this format:\n"
+    "Album Name - Artist Name"
+)
 
-SINGLE_MESSAGE = '''Please send the name of the song like this:
-Song Name
-or for better search, use this format:
-Song Name - Artist Name'''
+# Stickers and emojis
+NOT_FOUND_STICKER = "CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ"
 
-ALBUM_MESSAGE = '''Please send the name of the album like this:
-Album Name
-or for better search, use this format:
-Album Name - Artist Name'''
+# Status messages
+NOT_IN_DB = "❌💾 Song is not in database, this might take longer"
+DOWNLOADING = "downloading..."
+UPLOADING = "uploading..."
+PROCESSING = "processing..."
+ALREADY_IN_DB = "💾already in database"
+NO_LYRICS_FOUND = "❌No lyrics found"
+SONG_NOT_FOUND = "❌Song Not Found"
 
-NOT_FOUND_STICKER = 'CAACAgQAAxkBAAIFSWBF_m3GHUtZJxQzobvD_iWxYVClAAJuAgACh4hSOhXuVi2-7-xQHgQ'
-
-NOT_IN_DB = '❌💾 Song is not in database, this might take longer'
-
-DOWNLOADING = 'downloading...'
-
-UPLOADING = 'uploading...'
-
-PROCESSING = 'processing...'
-
-ALREADY_IN_DB = '💾already in database'
-
-NO_LYRICS_FOUND = '❌No lyrics found'
-
-SONG_NOT_FOUND = '❌Song Not Found'
-
-ALBUM_HAS_SENT_SUCCESSFULLY = '🎧album sent!'
-
-PLAYLIST_HAS_SENT_SUCCESSFULLY = '🎧playlist first 50 songs has sent!'
+# Success messages
+ALBUM_HAS_SENT_SUCCESSFULLY = "🎧album sent!"
+PLAYLIST_HAS_SENT_SUCCESSFULLY = "🎧playlist first 50 songs has sent!"
